bf38890c497828782b2099f0a1d7cb633520949a
MarcoAurelio <maurelio@tools.wmflabs.org>
2018-01-26 11:20:16 +0000

    Update licensing to GPL-3.0-or-later
    Bug: T184847
    Change-Id: I0a50033a92c2e0d9a84948b4c707c9c94d91503f

--- Blackout.php
+++ Blackout.php
@@ -1,16 +1,21 @@
 <?php
 /**
  * MediaWiki extension to protest, and raise awareness of, Internet censorship.
- * Installation instructions can be found on
- * https://www.mediawiki.org/wiki/Extension:Blackout
  *
- * @addtogroup Extensions
- * @author John Du Hart and Gregory Varnum utilizing work by jorm and MediaWiki developers for the
- * Wikimedia Foundation's SOPA/PIPA protest
- * @license GPL
+ * Copyright (C) 2012, Gregory Varnum & John Du Hart
  *
- * Thank you for feedback, bug reporting and cleaning up code
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'Blackout' );
